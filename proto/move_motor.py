9afdd889960b6a9fa1e57143f3daceba39e68fed
Robert Diersing <rdiersin@usc.edu>
2019-01-23 15:52:01 -0800

    converted motor script to linux port

--- proto/move_motor.py
+++ proto/move_motor.py
@@ -3,10 +3,15 @@ from time import sleep
 
 # This port is only for windows, it will be something else with 
 # any other enviornment
-ser = serial.Serial('COM3', 9600)
+ser = serial.Serial('/dev/ttyACM0', 9600)
 
 # Move the x motor by 1000
-ser.write('xf1000'.encode())
+ser.write(b'xf1000')
 ser.flush()
+
+while True:
+    print(ser.readline())
+
+sleep(5)
 
 print('program terminated')
