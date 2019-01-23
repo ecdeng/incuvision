0fddba4736f8193042b0c8c44335a5a046f6b92b
Robert Diersing <rdiersin@usc.edu>
2019-01-22 22:11:12 -0800

    created proto comm server that works

--- proto/com_server_proto.py
+++ proto/com_server_proto.py
@@ -5,15 +5,20 @@ PORT = 42042
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+print('starting server on %s:%d' % (HOST, PORT))
+
 s.bind((HOST, PORT))
 s.listen()
-conn, addr = s.accept()
-
-print('connected to: ', addr)
 while True:
-    cmd = conn.recv(1024)
-    if not cmd:
-        break
-    print('recieved command: ', cmd)
-
+    conn, addr = s.accept()
+    print('connected to: ', addr)
+    while True:
+        cmd = conn.recv(1024)
+        if not cmd:
+            break
+        print('recieved command: ', cmd)
+        conn.sendall(b'OK')
+    print('broke connection with: ', addr)
+    conn.close()
+    
 s.close()
---
+++ proto/mock_cmd_sender.py
@@ -0,0 +1,15 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 42042
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('connecting to server')
+s.connect((HOST, PORT))
+
+print('sending command')
+s.sendall(b'cmd=xf1000')
+status = s.recv(1024)
+
+print('command sent with status: ', status)
+s.close()
