b0866487e685b9eca34d92d8eedf435dceb109ab
Robert Diersing <rdiersin@usc.edu>
2019-01-23 23:16:23 -0800

    created motor controller class

--- proto/move_motor.py
+++ proto/move_motor.py
@@ -1,14 +1,43 @@
 import serial
 from time import sleep
 
-# This port is only for windows, it will be something else with 
-# any other enviornment
-ser = serial.Serial('com3', 9600)
-sleep(2)
+poss_dirs = {'xf', 'xb', 'yf', 'yb', 'zf', 'zb'}
 
-print('writing motor control')
-# Move the x motor by 1000
-ser.write(b'xf1000')
-sleep(5)
+class MotorController:
+    def __init__(self, port, bps):
+        self.port = port
+        self.bps = bps
+    
+    def start(self):
+        print('starting motor controller')
+        self.ser = serial.Serial(self.port, self.bps)
+        sleep(2)
+        print('motor controller running')
+
+    def verify_cmd(self, command):
+        if not isinstance(command, str):
+            return False
+        command = str(command)
+        dir = command[:2]
+        amt = int(command[2:])
+        if dir not in poss_dirs:
+            return False
+        if amt < 0 or amt > 1e5:
+            return False
+        return True
+
+    def exec_cmd(self, command):
+        self.ser.write(command.encode())
+        sleep(5)
+
+    def stop(self):
+        self.ser.close()
+
+motor_controller = MotorController('com3', 9600)
+motor_controller.start()
+motor_controller.exec_cmd('xf1000')
+motor_controller.exec_cmd('xb1000')
+motor_controller.exec_cmd('yf1000')
+motor_controller.stop()
 
 print('program terminated')
