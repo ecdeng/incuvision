83132ff2d0ec2d6701031fdaa73a419aaf328ec0
Robert Diersing <rdiersin@usc.edu>
2019-01-27 18:35:53 -0800

    cleaning up

--- proto/stepper_motor_library/stepper_motor_library.ino
+++ proto/stepper_motor_library/stepper_motor_library.ino
@@ -23,47 +23,31 @@ void loop() {
   if(Serial.available() > 0) {
     // reading the incoming byte:
     com = Serial.readString();
-
-    Serial.println(com);
-
     line1 = com.substring(0, 2);
     line2 = com.substring(2, 7);
-
-    Serial.println(line1);
-    Serial.println(line2);
-
     inputtime = line2.toInt();
 
     if(line1=="xf") {
       // rotate motor X clockwise by inputtime
-      Serial.println("In xf and making pulses");
       stepperX.step(inputtime);
       delay(500);
     }
-
     if(line1 == "xb") {
       // rotate motor X counterclockwise by inputtime
-      Serial.println("In xb and making pulses");
       stepperX.step(-inputtime);
       delay(500);
     }
-
     if(line1 == "yf") {
       // rotate motor Y clockwise by inputtime
-      Serial.println("In yf and making pulses");
       stepperY.step(inputtime);
       delay(500);
     }
-
     if(line1 == "yb") {
       // rotate motor Y counterclockwise by inputtime
-      Serial.println("In yb and making pulses");
       stepperY.step(-inputtime);
       delay(500);
     }
-
     inputtime = 0;
-    
   }
   
   delay(1);
