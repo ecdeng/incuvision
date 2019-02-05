9266d5202dccf50f3f569ce9e35a7134149ee49e
Robert Diersing <rdiersin@usc.edu>
2019-02-04 18:24:31 -0800

    added testing suite for motor_controller

--- linux_server/motor_controller.py
+++ linux_server/motor_controller.py
@@ -8,14 +8,26 @@ class MotorController:
         self.bps = bps
         self.poss_dirs = {'xf', 'xb', 'yf', 'yb', 'zf', 'zb'}
 
-    # initiate communication with the arduino
+    # Initiate communication with the arduino
     def start(self):
         print('starting motor controller')
         self.ser = serial.Serial(self.port, self.bps)
         sleep(2)
         print('motor controller running')
 
-    # send command to arduino and return the response
+    # Tests to see if a command can be executed by exec_cmd
+    def valid_cmd(self, command):
+        if (type(command) is not str):
+            return False
+        dir = command[:2]
+        ammnt = command[2:]
+        if dir not in self.poss_dirs:
+            return False
+        if not ammnt.isdigit():
+            return False
+        return int(ammnt) < 1e6
+
+    # Send command to arduino and return the response
     def exec_cmd(self, command):
         self.ser.write(command.encode())
         sleep(5)
---
+++ linux_server/test_motor_controller.py
@@ -0,0 +1,28 @@
+import unittest
+import motor_controller
+
+class TestMotorController(unittest.TestCase):
+
+    def setUp(self):
+        self.controller = motor_controller.MotorController(None, None)
+
+    def test_valid_cmd_xf(self):
+        self.assertTrue(self.controller.valid_cmd('xf1000'))
+    
+    def test_valid_cmd_yf(self):
+        self.assertTrue(self.controller.valid_cmd('yf1000'))
+    
+    def test_invalid_cmd_wrong_dir(self):
+        self.assertFalse(self.controller.valid_cmd('pf1000'))
+    
+    def test_invalid_cmd_nodir(self):
+        self.assertFalse(self.controller.valid_cmd('1000'))
+
+    def test_invalid_cmd_negitive(self):
+        self.assertFalse(self.controller.valid_cmd('xf-1000'))
+
+    def test_invalid_cmd_too_big(self):
+        self.assertFalse(self.controller.valid_cmd('yf10000000000'))
+
+if __name__ == '__main__':
+    unittest.main()
