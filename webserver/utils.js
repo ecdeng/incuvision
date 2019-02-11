38778e4727d8de50f7995db45c23587b554ffa24
Robert Diersing <rdiersin@usc.edu>
2019-02-10 22:41:46 -0800

    created utils.js and utils.test.js for testing whether or not a point string is valid

--- webserver/utils.js
+++ webserver/utils.js
@@ -3,14 +3,19 @@ const MAX_MOTOR_VAL = 50000;
 const isValidPointStr = function(pointStr) {
     console.log('in validate point function');
     pointStr = pointStr.trim();
-    pointStr = pointStr.replace(/[()]/, '');
+    if (pointStr.length < 5 || 
+        pointStr[0] != '(' || 
+        pointStr[pointStr.length - 1] != ')') {
+        return false;
+    }
+    pointStr = pointStr.substring(1, pointStr.length - 1);
     pointArr = pointStr.split(',');
     if (pointArr.length != 2) {
         console.log('Error: The point to move to is not a tuple');
         return false;
     }
     xStr = pointArr[0].trim();
-    yStr = pointArr[0].trim();
+    yStr = pointArr[1].trim();
     console.log('xStr: ' + xStr + ', yStr: ' + yStr);
 
     if (isNaN(xStr) || isNaN(yStr)) {
@@ -20,6 +25,7 @@ const isValidPointStr = function(pointStr) {
 
     x = parseInt(xStr);
     y = parseInt(yStr);
+    console.log('x: ', x, ', y: ', y);
     if (Math.abs(x) > MAX_MOTOR_VAL || Math.abs(y) > MAX_MOTOR_VAL) {
         console.log('Error: The point to move to is out of range');
         return false;
