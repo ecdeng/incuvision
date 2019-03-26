fad858cefebe765d044318df06a7bb1cfe073cef
Robert Diersing <rdiersin@usc.edu>
2019-03-25 20:58:59 -0700

    fixed test refactor

--- webserver/test/utils.test.js
+++ webserver/test/utils.test.js
@@ -1,4 +1,4 @@
-const isValidPointStr = require('../utils.js')
+const isValidPointStr = require('../src/utils.js').isValidPointStr;
 
 test('tests if the string "(10, 10)" is a point', () => {
     expect(isValidPointStr('(10, 10)')).toBe(true);
