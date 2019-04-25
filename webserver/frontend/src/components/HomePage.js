55dec5ddca9f386c94ccca35aa3a8a4485d57165
Patrick Truong <patricct@usc.edu>
2019-04-25 13:53:35 -0700

    switched to shaka player for better video streaming support

--- webserver/frontend/src/components/HomePage.js
+++ webserver/frontend/src/components/HomePage.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import '../styles/home.css';
 import socketIOClient from 'socket.io-client';
+import shaka from 'shaka-player'
 import videojs from 'video.js'
+<<<<<<< HEAD
+import Hls from 'hls.js';
+import axios from 'axios'
 
 class HomePage extends React.Component {
 	constructor() {
@@ -27,6 +31,7 @@ class HomePage extends React.Component {
 			}
 			this.setState({ message_status: msgStatus });
 		});
+		this.createVideoStream();
 	}
 
 	// destroy player on unmount
@@ -59,8 +64,8 @@ class HomePage extends React.Component {
 		var streamName = "Incuvision";
 		// Step 1: Configure SDK Clients
 		var options = {
-			accessKeyId: "AKIAJHVRF5Q3V2J4NK7Q",
-			secretAccessKey: "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",
+			accessKeyId: "access key",
+			secretAccessKey: "secret key",
 			region: "us-west-2"
 		}
 		var kinesisVideo = new AWS.KinesisVideo(options);
@@ -80,14 +85,87 @@ class HomePage extends React.Component {
 			kinesisVideoArchivedContent.getHLSStreamingSessionURL({
 				StreamName: streamName,
 				PlaybackMode: 'LIVE',
+				//PlaybackMode: 'ON_DEMAND'
 				HLSFragmentSelector: {
 					FragmentSelectorType: 'SERVER_TIMESTAMP',
 					TimestampRange: undefined
+					//TimestampRange: {
+					//	StartTimestamp: new Date('April 25, 2019 11:30:00'),
+                    //    EndTimestamp: new Date('April 25, 2019 11:35:00')
+                    //}
 					//ContainerFormat: "FRAGMENTED_MP4",
 					//DiscontinuityMode: "ALWAYS",
 					//DisplayFragmentTimestamp: "ALWAYS",
 					//MaxMediaPlaylistFragmentResults: undefined,
 					//Expires: undefined
+					},
+					Expires: 43200
+			}, function(err, response) {
+					if (err) { 
+						console.log("error retrieving HLSStreamingSessionURL");
+						console.log(err, err.stack); // an error occurred
+						var playerElement = document.getElementById("videojs");
+						const videoJsOptions = {
+  							autoplay: true,
+  							controls: true,
+  							sources: [{
+    							src: "//vjs.zencdn.net/v/oceans.mp4",
+    							type: "video/mp4"
+    							//src: response.HLSStreamingSessionURL,
+    							//type: 'application/x-mpegURL'
+  							}]
+						}
+    					this.player = videojs(playerElement, videoJsOptions, function onPlayerReady() {
+      						console.log('onPlayerReady', this)
+    					});
+						console.log('Set player source');
+						this.player.play();
+						console.log('Starting playback of oceans');
+						return console.error(err); 
+					}
+					console.log('HLS Streaming Session URL: ' + response.HLSStreamingSessionURL);
+					// Step 4: Give the URL to the video player.
+					//var video = document.getElementById("video");
+					//var video = this.player;
+					
+					var playerElement = document.getElementById("shaka");
+                    var player = new shaka.Player(playerElement);
+					console.log('Created Shaka Player');
+                    player.load(response.HLSStreamingSessionURL).then(function() {
+                        console.log('Starting playback');
+                    });
+					console.log('Set player source');
+					playerElement.play();
+					
+                    
+					/**
+					var player = new Hls();
+					if(Hls.isSupported()) {
+						console.log('HLS is supported!');	
+						player.loadSource(response.HLSStreamingSessionURL);
+                    	player.attachMedia(video);
+						console.log('Set player source');
+						player.on(Hls.Events.MANIFEST_PARSED, function() {
+							video.play();
+                        	console.log('Starting playback');
+                    	});
+    				}
+
+    				player.on(Hls.Events.ERROR, function (event, data) {
+   						var errorType = data.type;
+    					var errorDetails = data.details;
+    					var errorFatal = data.fatal;
+    					console.log('Error');	
+    					console.log('error type is: ' + errorType);
+    					console.log('error details are: ' + errorDetails);
+    					console.log('error fatal: ' + errorFatal);
+    					//console.log('stack trace: ' + data.err.stack);
+
+  					});
+					**/
+  					
+                    
+					/**
 				},
 				Expires: 300
 			}, function (err, response) {
@@ -105,6 +183,13 @@ class HomePage extends React.Component {
 							//type: 'application/x-mpegURL'
 						}]
 					}
+    				this.player = videojs(playerElement, videoJsOptions, function onPlayerReady() {
+
+    				});
+    				**/
+					//console.log('Set player source');
+					//this.player.play();
+					//console.log('Starting playback');
 					this.player = videojs(playerElement, videoJsOptions, function onPlayerReady() {
 						console.log('onPlayerReady', this)
 					});
@@ -146,6 +231,7 @@ class HomePage extends React.Component {
 
 	captureImage(experimentId) {
 		//capture a snapshot from the video js player: https://stackoverflow.com/questions/13760805/how-to-take-a-snapshot-of-html5-javascript-based-video-player
+		var video=document.querySelector('#shaka');
 		var video = document.querySelector('#videojs video');
 		console.log("creating canvas");
 		var canvas = document.createElement('canvas');
@@ -163,8 +249,8 @@ class HomePage extends React.Component {
 		console.log("Start uploading to s3");
 		var AWS = require('aws-sdk');
 		var config = new AWS.Config({
-			accessKeyId: "AKIAJHVRF5Q3V2J4NK7Q",
-			secretAccessKey: "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",
+			accessKeyId: "access key",
+			secretAccessKey: "secret key",
 			region: "us-west-2"
 		});
 		AWS.config = config;
@@ -183,6 +269,25 @@ class HomePage extends React.Component {
 			ContentType: 'image/jpeg'
 		};
 		console.log("Store into s3");
+		s3Bucket.putObject(data, function(err, data){
+      		if (err) { 
+       			console.log(err);
+        		console.log('Error uploading photo to s3'); 
+      		} else {
+        		console.log('succesfully uploaded the image!');
+      		}
+  		});
+  		var urlParams = {Bucket: 'incuvision', Key: uuid};
+  		var s3 = new AWS.S3();
+  		console.log("Get presigned URL");
+  		s3.getSignedUrl('getObject', urlParams, function(err, url) {
+  			console.log("Signed url is: " + url);
+  			axios.post("http://localhost:5000/images/create/", {
+  				name: (experimentId) ? "Experiment" + experimentId + "_" + d + ".jpeg" : "ManualCapture_" + d + ".jpeg",
+  				timestamp: d,
+  				filepath: url
+  			})
+  		});
 		s3Bucket.putObject(data, function (err, data) {
 			if (err) {
 				console.log(err);
@@ -213,10 +318,17 @@ class HomePage extends React.Component {
 				<div className="leftPane">
 					<div className="currentPos">Current position: <span className="posName">#3</span></div>
 					<div className="camera">
+						
+                        <video id="shaka" class="player" controls autoplay></video>
+                        <script src="https://cdnjs.cloudflare.com/ajax/libs/shaka-player/2.4.1/shaka-player.compiled.js"></script>
+						
+
+						/**
 						<video className="player video-js vjs-default-skin" id="videojs"></video>
 						<script src="https://vjs.zencdn.net/6.6.3/video.js"></script>
 						<script src="https://cdnjs.cloudflare.com/ajax/libs/videojs-contrib-hls/5.14.1/videojs-contrib-hls.js"></script>
 						<link rel="stylesheet" href="//vjs.zencdn.net/5.12/video-js.css" />
+						**/
 						{this.createVideoStream()}
 					</div>
 					<button className="photoCapture" onClick={this.takePhoto} >Take Photo</button>
